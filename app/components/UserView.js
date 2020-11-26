bd2496939234ba1f6eebb43ab0569eddaa7d70b8
Nidhin S G <nidhin@mobomo.com>
2020-11-26 20:41:49 +0530

    Rendered the looping List on another view

---
+++ app/components/UserView.js
@@ -0,0 +1,10 @@
+import Marionette from 'backbone.marionette';
+import template from '../templates/user.jst';
+
+
+var UserView = Marionette.View.extend({
+    template: template,
+    tagName: 'tr'   
+});
+
+export default UserView
--- app/components/UsersView.js
+++ app/components/UsersView.js
@@ -1,8 +1,16 @@
 import Marionette from 'backbone.marionette';
 import template from '../templates/user_table.jst';
+import UserView from './UserView'
 
 var UsersView = Marionette.View.extend({
-  template: template
+  template: template,
+
+  regions: {
+    tableRegion: '#user_listing_table'
+  },
+  onRender() {
+    this.showChildView('tableRegion', new UserView());
+  }
 });
 
 export default UsersView
