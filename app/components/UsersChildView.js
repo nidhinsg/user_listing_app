6b6cdb4db18499d0a720c8dea76f2415098e2caf
Nidhin S G <nidhin@mobomo.com>
2020-11-27 19:00:12 +0530

    View Rendering from collection try out

---
+++ app/components/UsersChildView.js
@@ -0,0 +1,15 @@
+import Marionette from 'backbone.marionette';
+import template from '../templates/user_table.jst';
+import UserView from './UserView'
+
+var UsersChildView = Marionette.View.extend({
+  template: template,
+  regions: {
+    tableRegion: '#user_listing_table'
+  },
+  onRender() {
+    this.showChildView('tableRegion', new UserView());
+  }
+});
+
+export default UsersChildView
