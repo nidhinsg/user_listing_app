8ca7dd7af2977e3815a8fa24fcbf2ba537bb7258
Nidhin S G <nidhin@mobomo.com>
2020-11-28 21:30:28 +0530

    CSV file upload to server using post api completed

--- app/components/FileUploadView.js
+++ app/components/FileUploadView.js
@@ -10,7 +10,46 @@ var UploadView = Marionette.View.extend({
     },
     onRender() {
     	this.showChildView('tableRegion', new UsersView());
+    },
+    events: {
+    	'submit form': 'uploadFile'
+    },
+    ui: {
+    	'file': '#myFile',
+    	'form': 'form'
+    },
+    uploadFile(event) {
+    	if(event){ event.preventDefault(); }
+    	var payload = new FormData(this.ui.form[0]);
+    	this.collection = uploadFile;
+	    this.collection.fetch({ data: payload, type:'POST', processData: false, contentType: false });
     }
 });
+
+var FileUpload = Backbone.Model.extend({
+	urlRoot: 'http://localhost:3000/users/upload_data'
+});
+
+// API Response Format
+// { : }
+
+var FileUploadCollection = Backbone.Collection.extend({
+    url: 'http://localhost:3000/users/upload_data.json',
+    model: FileUpload,
+    parse: function(response) {
+    	console.log(response);
+    	if (response.status) {
+    		$("#success_alert").html(response.message);
+    		$("#success_alert").show();
+    	} else {
+    		$("#error_alert").html(response.message);
+    		$("#error_alert").show();
+    	}
+    	
+    	return response
+    }
+});
+
+var uploadFile = new FileUploadCollection();
 
 export default UploadView
